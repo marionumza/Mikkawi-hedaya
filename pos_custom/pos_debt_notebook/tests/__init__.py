5f6a336311b036a540ea83e0b4d5a1aef2dc1670
ahmadnader83@yahoo.com <ahmadnader83@yahoo.com>
2019-07-08 18:55:49 +0300

    fix

--- pos_custom/pos_debt_notebook/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
-
-from . import test_newbalance
-from . import test_pos_debt
--- pos_custom/pos_debt_notebook/wizard/__init__.py
+++
@@ -1,4 +0,0 @@
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
-
-from . import pos_credit_invoices
-from . import pos_credit_company_invoices
